59a7f47a7b58ce920835076ddb4f7a8a728c4184
vedaprathikgattu <gattuvedaprathikgoud@gmail.com>
2025-09-25 23:14:06 +0530

    info updated

---
+++ Portfolio1/src/Data/info.js
@@ -0,0 +1,11 @@
+import photo from './photo';
+
+const info={
+    main: {
+        name: "VedaPrathik Gattu";
+        description: "Motivated Computer Science Engineering student with hands-on experience in innovative projects by applying my expertise in backend development with Spring Boot, frontend technologies like React and Bootstrap, and database management with MySQL. Seeking a Software Engineer role to contribute to innovative projects by leveraging skills in Java, Python, React, Spring Boot, Bootstrap, and MySQL to build scalable and efficient software solutions.";
+        role: "Software Engineer";
+        photo: "photo";
+        email: "gattuvedaprathikgoud@gmail.com";
+    }
+}
